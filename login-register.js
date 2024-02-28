9cd7c4d4665f3cb2ee4e7b574cd4c71e2d059a02
Riley Ballard <rileyrb01@gmail.com>
2024-02-28 14:22:06 -0700

    add register function with localstorage

--- login-register.js
+++ login-register.js
@@ -1,8 +1,13 @@
-document.getElementById("myForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-  
-    var formData = new FormData(form);
-    // output as an object
-    console.log(Object.fromEntries(formData));
+function register() {
+  var username_ = document.querySelector("#username-r");
+  var password_ = document.querySelector("#password-r");
+  var full_name_ = document.querySelector("#full-name-r");
+  localStorage.setItem("new_user", JSON.stringify({name: full_name_.value, username: username_.value, password: password_.value}))
+  window.location.href = "employee-form.html";
+}
 
-  });
+function login() {
+  var username_ = document.querySelector("#username");
+  var password_ = document.querySelector("#password");
+
+}
