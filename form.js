49327dd10a48244671b48b7ab01c4dd6dd256d56
Riley Ballard <rileyrb01@gmail.com>
2024-02-28 18:38:00 -0700

    add form submission into a mock database

--- form.js
+++ form.js
@@ -1,17 +1,22 @@
+var mock_database = []
+
+var user = JSON.parse(localStorage.getItem("new_user"));
+
 window.onload = function() {
-    var user = JSON.parse(localStorage.getItem("new_user"));
     document.getElementById("full_name").innerHTML = "Welcome back, " + user.name;
+
 }
 
 function submit_form() {
-    var goals = document.querySelector("#goals");
+    var goals_ = document.querySelector("#goals");
     var radio_buttons = document.getElementsByName("goal_rate")
+    var checked_button;
     var next_goals = document.querySelector("#next_goals");
-    console.log(goals.value)
     for (let radio of radio_buttons) {
         if (radio.checked) {
-            console.log(radio.value)
+            checked_button = radio.value;
         }
     }
-    console.log(next_goals.value)
+    mock_database.push({name: user.name, goals: goals_.value, goal_rate: checked_button, next_goals: next_goals.value, form_complete: false})
+    console.log(mock_database)
 }
