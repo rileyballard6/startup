b9d7469f4fe1e1323e7ea1f44498495f189dad5a
Riley Ballard <rileyrb01@gmail.com>
2024-03-11 08:58:04 -0600

    added mock database to backend and changing register to POST to backend for registration

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+app.use(express.json());
+app.use(express.static('public'));
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
