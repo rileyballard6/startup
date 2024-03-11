c7015053fff84395773193bdd2f54bab97fa55ee
Riley Ballard <rileyrb01@gmail.com>
2024-03-11 09:08:37 -0600

    changing form to backend database

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const { name } = require('ejs');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+var mock_database = [];
+
+app.post('/register', (req,res) => {
+    var name = req.body;
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log(name, username, password);
+    res.redirect('/')
+})
 
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
