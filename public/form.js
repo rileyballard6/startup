f11672196f3a2663d8bb959e3dcf4afbdcb23858
Riley Ballard <rileyrb01@gmail.com>
2024-03-29 10:16:34 -0600

    add websocket connection to form.js to send data when a form is submitted

--- public/form.js
+++ public/form.js
@@ -1,10 +1,16 @@
-var mock_database = []
-
+const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+socket.onopen = (event) => {
+    console.log("the web socket is open");
+  };
+  socket.onclose = (event) => {
+    console.log("the web socket is closed");
+  };
 
 window.onload = async function() {
 
     const response = await fetch("https://official-joke-api.appspot.com/random_joke");
-    const user = await fetch("https://startup.cs260checkin.click/logininfo");
+    const user = await fetch("http://localhost:3000/logininfo");
     const user_data = await user.json();
     const jsondata = await response.json();
     document.getElementById("full_name").innerHTML = "Welcome back, " + user_data.user.full_name;
@@ -14,17 +20,11 @@ window.onload = async function() {
 }
 
 // gather the responses from the form and add them to the database so that the manager can see them in the real application
-function submit_form() {
-    var goals_ = document.querySelector("#goals");
-    var radio_buttons = document.getElementsByName("goal_rate")
-    var checked_button;
-    var next_goals = document.querySelector("#next_goals");
-    for (let radio of radio_buttons) {
-        if (radio.checked) {
-            checked_button = radio.value;
-        }
-    }
-    mock_database.push({name: user.name, goals: goals_.value, goal_rate: checked_button, next_goals: next_goals.value, form_complete: false})
 
-    document.getElementById("full_name").innerHTML = "Thank you for submitting!";
+function sendMessage() {
+    socket.send("form submitted!!");
+      socket.onmessage = async (event) => {
+        const msg = JSON.parse(await event.data.text());
+        console.log(msg);
+    }
 }
