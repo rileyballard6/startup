aa01afc08958bd9b70a77ababa1147b1ec76194c
Riley Ballard <rileyballard@RileyWokMacbook.app.byu.edu>
2024-04-10 13:57:40 -0600

    imported HomeScreen and added to App.jsx

--- demoVite/src/App.jsx
+++ demoVite/src/App.jsx
@@ -1,38 +1,12 @@
 import { useState } from "react";
-import meeting from "./assets/meeting.png"
+import HomeScreen from "./components/index.jsx";
 
 import "./App.css";
 
 function App() {
-
   return (
     <>
-      <body>
-        <header class="header-info">
-          <h2>Check-in Application</h2>
-          <nav>
-            <a href="register.html">Register</a>
-            <a href="login.html">Login</a>
-          </nav>
-        </header>
-        <div class="middle-info">
-          <h1>Perfomance reviews made easier!</h1>
-          <p>
-            Your ultimate solution for streamlined and effective performance
-            reviews! Say goodbye to the hassle of traditional review processes
-            and embrace a user-friendly platform designed to make performance
-            evaluations a breeze.
-          </p>
-          <img src={meeting} alt="business meeting" />
-          <a href="register.html">Register Today</a>
-        </div>
-        <footer>
-          <p>Riley Ballard</p>
-          <a href="https://github.com/rileyballard6/startup">
-            Github Repository
-          </a>
-        </footer>
-      </body>
+      <HomeScreen />
     </>
   );
 }
