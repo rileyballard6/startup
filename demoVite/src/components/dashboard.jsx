abee4dde8b071ffc9f0bd5bf8190329881f21c6e
Riley Ballard <rileyballard@RileyWokMacbook.app.byu.edu>
2024-04-11 14:32:43 -0600

    add dashboard jsx and transfer style sheets over

---
+++ demoVite/src/components/dashboard.jsx
@@ -0,0 +1,50 @@
+import "../dashboard.css"
+
+export default function Dashboard() {
+    return (
+        <body>
+        <header class="header-info">
+          <h2>Check-in Application</h2>
+          <nav>
+            <a href="index.html">Log-out</a>
+          </nav>
+        </header>
+        <div class="middle-dashboard">
+            <h1 id="full_name">Welcome back,</h1>
+            <p id="web-socket-message"></p>
+            <div class="dashboard-information">
+                <div class="info">
+                    <h3 id="data_number"></h3>
+                    <h3>Current Employees</h3>
+                </div>
+                <div class="info">
+                    <h3 id="review_number"></h3>
+                    <h3>Forms to review</h3>
+                </div>
+                <div class="info">
+                    <h3 id="complete_number"></h3>
+                    <h3>Forms complete</h3>
+                </div>
+                <div class="dashboard-information-employee">
+                  <h3>Forms to review:</h3>
+                  <div class="employee-form" id="employee-form">
+                    <button onclick="complete_form()" id="complete_form_button">Complete Form</button>
+                    <h3>Employee Name</h3>
+                    <div class="form_results">
+                      <p>Answer 1</p>
+                      <p>Answer 2</p>
+                      <p>Answer 3</p>
+                    </div>
+                  </div>
+                  
+                </div>
+            </div>
+            
+        </div>
+        <footer>
+          <p>Riley Ballard</p>
+            <a href="https://github.com/rileyballard6/startup">Github Repository</a>
+        </footer>
+      </body>
+    )
+}
--- demoVite/src/components/register.jsx
+++ demoVite/src/components/register.jsx
@@ -1,20 +1,22 @@
-import "../login.css"
+import "../login.css";
 
 export default function Register() {
-  <div class="register-form">
-    <div class="form">
-      <form action="/register" method="post">
-        <h1>Welcome to Checkin Application!</h1>
-        <label for="full-name">Enter Full Name</label>
-        <input type="text" name="full_name" id="full-name-r" />
-        <label for="username">Enter Username</label>
-        <input type="text" name="username" id="username-r" />
-        <label for="password">Enter Password</label>
-        <input type="password" name="password" id="password-r" />
-        <button type="submit" onclick="register()">
-          Register
-        </button>
-      </form>
+  return (
+    <div class="register-form">
+      <div class="form">
+        <form action="/register" method="post">
+          <h1>Welcome to Checkin Application!</h1>
+          <label for="full-name">Enter Full Name</label>
+          <input type="text" name="full_name" id="full-name-r" />
+          <label for="username">Enter Username</label>
+          <input type="text" name="username" id="username-r" />
+          <label for="password">Enter Password</label>
+          <input type="password" name="password" id="password-r" />
+          <button type="submit" onclick="register()">
+            Register
+          </button>
+        </form>
+      </div>
     </div>
-  </div>;
+  );
 }
